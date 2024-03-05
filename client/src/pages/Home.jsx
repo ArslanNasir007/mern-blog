b445db78cda48559585c1328a24d71ffaaa53583
Arslan Nasir <fromarslannasir@gmail.com>
2024-03-05 15:22:39 +0500

    Update Home.jsx
    use max width on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ useEffect(() => {
 
   return (
     <div>
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-7xl mx-auto">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold lg:text-6xl">Welcome to Arslan Blog</h1>
         <p className="text-gray-500 text-xs sm:text-sm">Here you will find a variety of articles and tutorials on topics such as
           web development, software engineering, and programming languages.</p>
